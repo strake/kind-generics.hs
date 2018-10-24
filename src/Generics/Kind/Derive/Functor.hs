e5445b4f2790ac70b2eb9d07355f5bc6dd794aa7
Alejandro Serrano <trupill@gmail.com>
2018-10-24 23:29:23 +0200

    Final Functor generic deriving

--- src/Generics/Kind/Derive/Functor.hs
+++ src/Generics/Kind/Derive/Functor.hs
@@ -1,39 +1,39 @@
-{-# language DataKinds             #-}
-{-# language PolyKinds             #-}
-{-# language GADTs                 #-}
-{-# language RankNTypes            #-}
-{-# language TypeOperators         #-}
-{-# language MultiParamTypeClasses #-}
-{-# language FlexibleInstances     #-}
-{-# language FlexibleContexts      #-}
-{-# language QuantifiedConstraints #-}
-{-# language UndecidableInstances  #-}
+{-# language DataKinds              #-}
+{-# language PolyKinds              #-}
+{-# language GADTs                  #-}
+{-# language RankNTypes             #-}
+{-# language TypeOperators          #-}
+{-# language MultiParamTypeClasses  #-}
+{-# language FlexibleInstances      #-}
+{-# language FlexibleContexts       #-}
+{-# language QuantifiedConstraints  #-}
+{-# language UndecidableInstances   #-}
+{-# language ScopedTypeVariables    #-}
+{-# language FunctionalDependencies #-}
 module Generics.Kind.Derive.Functor where
+
+import Data.Proxy
 
 import Generics.Kind
 
-data Variance k where
-  End     :: Variance (*)
-  Co      :: Variance ks -> Variance (* -> ks)
-  Contra  :: Variance ks -> Variance (* -> ks)
-  Fixed   :: Variance ks -> Variance (k -> ks)
-  Phantom :: Variance ks -> Variance (k -> ks)
-  Higher  :: Variance k -> Variance ks -> Variance (k -> ks)
+data Variance = Co | Contra | Fixed | Phantom
+type Variances = [Variance]
 
-data Mappings (v :: Variance k) (x :: LoT k) (y :: LoT k) where
-  EndM     :: Mappings End LoT0 LoT0
-  CoM      :: (a -> b) -> Mappings vs as bs
-           -> Mappings (Co      vs)  (a :&&: as) (b :&&: bs)
-  ContraM  :: (b -> a) -> Mappings vs as bs
-           -> Mappings (Contra  vs)  (a :&&: as) (b :&&: bs)
-  FixedM   ::             Mappings vs as bs
-           -> Mappings (Fixed   vs)  (t :&&: as) (t :&&: bs)
-  PhantomM ::             Mappings vs as bs
-           -> Mappings (Phantom vs)  (a :&&: as) (b :&&: bs)
-  HigherM  :: (forall cs ds. Mappings v cs ds) -> Mappings vs as bs
-           -> Mappings (Higher v vs) (a :&&: as) (b :&&: bs)
+data Mapping (v :: Variance) (a :: k) (b :: k) where
+  CoM      :: (a -> b) -> Mapping Co      a b
+  ContraM  :: (b -> a) -> Mapping Contra  a b
+  FixedM   ::             Mapping Fixed   t t
+  PhantomM ::             Mapping Phantom a b
 
-class GFunctor (f :: LoT k -> *) (v :: Variance k) (as :: LoT k) (bs :: LoT k) where
+data Mappings (v :: Variances) (x :: LoT k) (y :: LoT k) where
+  Map0 :: Mappings '[] LoT0 LoT0
+  MapC :: Mapping v a b -> Mappings vs as bs
+       -> Mappings (v ': vs) (a :&&: as) (b :&&: bs)
+
+class KFunctor (f :: k) (v :: Variances) | f -> v where
+  kfmap :: Mappings v as bs -> f :@@: as -> f :@@: bs
+
+class GFunctor (f :: LoT k -> *) (v :: Variances) (as :: LoT k) (bs :: LoT k) where
   gfmap :: Mappings v as bs -> f as -> f bs
 
 instance GFunctor U1 v as bs where
@@ -55,6 +55,54 @@ instance (Ty c as => GFunctor f v as bs, Ty c bs)
          => GFunctor (c :=>: f) v as bs where
   gfmap v (C x) = C (gfmap v x)
 
-instance (forall t. GFunctor f (Fixed v) (t :&&: as) (t :&&: bs))
+instance (forall t. GFunctor f (Fixed ': v) (t :&&: as) (t :&&: bs))
          => GFunctor (E f) v as bs where
-  gfmap v (E x) = E (gfmap (FixedM v) x)
+  gfmap v (E x) = E (gfmap (MapC FixedM v) x)
+
+class GFunctorField (t :: Atom d (*)) (v :: Variances) (as :: LoT d) (bs :: LoT d) where
+  gfmapf :: Proxy t -> Mappings v as bs -> Ty t as -> Ty t bs
+
+class GFunctorHead (f :: Atom d k)
+                   (vin :: Variances) (vout :: Variances) 
+                   (as :: LoT d) (bs :: LoT d)
+                   (rs :: LoT k) (ts :: LoT k) where
+  gfmaph :: Proxy f
+         -> Mappings vin as bs -> Mappings vout rs ts
+         -> Ty f as :@@: rs -> Ty f bs :@@: ts
+
+instance forall t v as bs. GFunctorField t v as bs
+         => GFunctor (F t) v as bs where
+  gfmap v (F x) = F (gfmapf (Proxy :: Proxy t) v x)
+
+instance GFunctorField (Kon t) v as bs where
+  gfmapf _ _ x = x
+
+instance GFunctorField (Var VZ) (Co ': v) (a :&&: as) (b :&&: bs) where
+  gfmapf _ (MapC (CoM f) _) x = f x
+
+instance forall vr pre v a as b bs.
+         GFunctorField (Var vr) v as bs
+         => GFunctorField (Var (VS vr)) (pre ': v) (a :&&: as) (b :&&: bs) where
+  gfmapf _ (MapC _ rest) x = gfmapf (Proxy :: Proxy (Var vr)) rest x
+
+instance forall f x v as bs.
+         ( GFunctorHead f v '[Co] as bs (Ty x as :&&: LoT0) (Ty x bs :&&: LoT0)
+         , GFunctorField x v as bs )
+         => GFunctorField (f :@: x) v as bs where
+  gfmapf _ v x = unA0 $ unArg
+               $ gfmaph (Proxy :: Proxy f) v (MapC (CoM (gfmapf (Proxy :: Proxy x) v)) Map0)
+               $ Arg $ A0 x
+
+instance forall f x v acc as bs rs ts.
+         ( GFunctorHead f v (Co ': acc) as bs (Ty x as :&&: rs) (Ty x bs :&&: ts)
+         , GFunctorField x v as bs )
+         => GFunctorHead (f :@: x) v acc as bs rs ts where
+  gfmaph _ v acc x = unArg
+                   $ gfmaph (Proxy :: Proxy f) v (MapC (CoM (gfmapf (Proxy :: Proxy x) v)) acc)
+                   $ Arg $ x
+
+instance KFunctor f acc => GFunctorHead (Kon f) v acc as bs ts rs where
+  gfmaph _ _ acc x = kfmap acc x
+
+instance KFunctor (Ty (Var vr) os) acc => GFunctorHead (Var vr) v acc os os ts rs where
+  gfmaph _ _ acc x = kfmap acc x
