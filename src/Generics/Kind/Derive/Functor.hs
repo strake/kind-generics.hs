d668d35112b30cd8b7ce35d2cf572e82e8c5d965
Alejandro Serrano <trupill@gmail.com>
2018-10-25 07:50:58 +0200

    More on functor

--- src/Generics/Kind/Derive/Functor.hs
+++ src/Generics/Kind/Derive/Functor.hs
@@ -11,6 +11,7 @@
 {-# language ScopedTypeVariables    #-}
 {-# language FunctionalDependencies #-}
 {-# language TypeApplications       #-}
+{-# language DefaultSignatures      #-}
 module Generics.Kind.Derive.Functor where
 
 import Data.Proxy
@@ -26,17 +27,24 @@ data Mapping (v :: Variance) (a :: k) (b :: k) where
   FixedM   ::             Mapping Fixed   t t
   PhantomM ::             Mapping Phantom a b
 
+infixr 5 :^:
 data Mappings (v :: Variances) (x :: LoT k) (y :: LoT k) where
-  Map0 :: Mappings '[] LoT0 LoT0
-  MapC :: Mapping v a b -> Mappings vs as bs
-       -> Mappings (v ': vs) (a :&&: as) (b :&&: bs)
+  M0    :: Mappings '[] LoT0 LoT0
+  (:^:) :: Mapping v a b -> Mappings vs as bs
+        -> Mappings (v ': vs) (a :&&: as) (b :&&: bs)
 
-class KFunctor (f :: k) (v :: Variances) | f -> v where
+class KFunctor (f :: k) (v :: Variances) (as :: LoT k) (bs :: LoT k) | f -> v where
   kfmap :: Mappings v as bs -> f :@@: as -> f :@@: bs
 
-kfmapDef :: (GenericK f, GFunctor (RepK f) v as bs, SForLoT as, SForLoT bs)
-         => Mappings v as bs -> f :@@: as -> f :@@: bs
-kfmapDef v = toK slot . gfmap v . fromK slot
+  default
+    kfmap :: (GenericK f, GFunctor (RepK f) v as bs, SForLoT as, SForLoT bs)
+          => Mappings v as bs -> f :@@: as -> f :@@: bs
+  kfmap v = toK slot . gfmap v . fromK slot
+
+kfmap1 :: forall f t v a b.
+          (KFunctor (f :: k -> *) (v :: Variances) (a :&&: LoT0) (b :&&: LoT0))
+       => Mappings v (a :&&: LoT0) (b :&&: LoT0) -> f a -> f b
+kfmap1 v = unravel . kfmap v . ravel
 
 class GFunctor (f :: LoT k -> *) (v :: Variances) (as :: LoT k) (bs :: LoT k) where
   gfmap :: Mappings v as bs -> f as -> f bs
@@ -62,7 +70,7 @@ instance (Ty c as => GFunctor f v as bs, Ty c bs)
 
 instance (forall t. GFunctor f (Fixed ': v) (t :&&: as) (t :&&: bs))
          => GFunctor (E f) v as bs where
-  gfmap v (E x) = E (gfmap (MapC FixedM v) x)
+  gfmap v (E x) = E (gfmap (FixedM :^: v) x)
 
 class GFunctorArg (t :: Atom d (*))
                   (v :: Variances) (intended :: Variance)
@@ -84,38 +92,40 @@ instance GFunctorArg (Kon t) v Fixed as bs where
   gfmapf _ _ _ = FixedM
 
 instance GFunctorArg (Var VZ) (Co ': v) Co (a :&&: as) (b :&&: bs) where
-  gfmapf _ _ (MapC (CoM f) _) = CoM f
+  gfmapf _ _ (CoM f :^: _) = CoM f
 instance GFunctorArg (Var VZ) (Contra ': v) Contra (a :&&: as) (b :&&: bs) where
-  gfmapf _ _ (MapC (ContraM f) _) = ContraM f
+  gfmapf _ _ (ContraM f :^: _) = ContraM f
 instance GFunctorArg (Var VZ) (Fixed ': v) Fixed (a :&&: as) (b :&&: bs) where
-  gfmapf _ _ (MapC FixedM _) = FixedM
+  gfmapf _ _ (FixedM :^: _) = FixedM
 
 instance forall vr pre v intended a as b bs.
          GFunctorArg (Var vr) v intended as bs
          => GFunctorArg (Var (VS vr)) (pre ': v) intended (a :&&: as) (b :&&: bs) where
-  gfmapf _ _ (MapC _ rest) = gfmapf (Proxy @(Var vr)) (Proxy @intended) rest
+  gfmapf _ _ (_ :^: rest) = gfmapf (Proxy @(Var vr)) (Proxy @intended) rest
 
 instance forall f x v v1 as bs.
-         (KFunctor f '[v1], GFunctorArg x v v1 as bs)
+         (KFunctor f '[v1] (Ty x as :&&: LoT0) (Ty x bs :&&: LoT0),
+          GFunctorArg x v v1 as bs)
          => GFunctorArg (f :$: x) v Co as bs where
-  gfmapf _ _ v = CoM (unA0 . unArg . kfmap (MapC (gfmapf (Proxy @x) (Proxy @v1) v) Map0) . Arg . A0)
+  gfmapf _ _ v = CoM (unA0 . unArg . kfmap (gfmapf (Proxy @x) (Proxy @v1) v :^: M0) . Arg . A0)
 
 instance forall f x y v v1 v2 as bs.
-         (KFunctor f '[v1, v2], GFunctorArg x v v1 as bs, GFunctorArg y v v2 as bs)
+         (KFunctor f '[v1, v2] (Ty x as :&&: Ty y as :&&: LoT0) (Ty x bs :&&: Ty y bs :&&: LoT0),
+          GFunctorArg x v v1 as bs, GFunctorArg y v v2 as bs)
          => GFunctorArg (f :$: x :@: y) v Co as bs where
   gfmapf _ _ v = CoM ( unA0 . unArg . unArg .
-                       kfmap (MapC (gfmapf (Proxy @x) (Proxy @v1) v)
-                             (MapC (gfmapf (Proxy @y) (Proxy @v2) v)
-                             Map0)) . 
+                       kfmap (gfmapf (Proxy @x) (Proxy @v1) v :^:
+                              gfmapf (Proxy @y) (Proxy @v2) v :^: M0) . 
                        Arg . Arg . A0 )
 
+{-
 instance forall f x y z v v1 v2 v3 as bs.
          (KFunctor f '[v1, v2, v3], 
           GFunctorArg x v v1 as bs, GFunctorArg y v v2 as bs, GFunctorArg z v v3 as bs)
          => GFunctorArg (f :$: x :@: y :@: z) v Co as bs where
   gfmapf _ _ v = CoM ( unA0 . unArg . unArg . unArg .
-                       kfmap (MapC (gfmapf (Proxy @x) (Proxy @v1) v)
-                             (MapC (gfmapf (Proxy @y) (Proxy @v2) v)
-                             (MapC (gfmapf (Proxy @z) (Proxy @v3) v)
-                             Map0))) . 
+                       kfmap (gfmapf (Proxy @x) (Proxy @v1) v :^:
+                              gfmapf (Proxy @y) (Proxy @v2) v :^:
+                              gfmapf (Proxy @z) (Proxy @v3) v :^: M0) . 
                        Arg . Arg . Arg . A0 )
+-}
--- src/Generics/Kind/Examples.hs
+++ src/Generics/Kind/Examples.hs
@@ -1,15 +1,20 @@
-{-# language TypeOperators #-}
-{-# language TypeFamilies  #-}
-{-# language DataKinds     #-}
+{-# language TypeOperators         #-}
+{-# language TypeFamilies          #-}
+{-# language DataKinds             #-}
+{-# language MultiParamTypeClasses #-}
+{-# language FlexibleInstances     #-}
 module Generics.Kind.Examples where
 
 import Data.Proxy
 import GHC.Generics (from, to)
 
 import Generics.Kind
+import Generics.Kind.Derive.Functor
 
 instance GenericK Maybe where
   type RepK Maybe = U1 :+: F V0
 
   fromK SLoT1 = fromKDefault
   toK   SLoT1 = toKDefault
+
+instance KFunctor Maybe '[Co] (a :&&: LoT0) (b :&&: LoT0) where
